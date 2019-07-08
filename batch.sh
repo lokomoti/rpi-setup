66ee319d37c26262bf85e89cc950eb7e1870049d
lokomoti <40903296+lokomoti@users.noreply.github.com>
2019-07-08 05:01:21 +0200

    Update batch.sh

--- batch.sh
+++ batch.sh
@@ -20,4 +20,29 @@ wget http://prdownloads.sourceforge.net/webadmin/webmin_1.920_all.deb
 sudo dpkg --install webmin_1.920_all.deb
 
 #install tools
-sudo apt-get install nmap tmux hwmon -y
+sudo apt-get install nmap tmux -y
+
+#install openCV4
+sudo apt-get install build-essential cmake unzip pkg-config -y
+sudo apt-get install libjpeg-dev libpng-dev libtiff-dev -y
+sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev -y
+sudo apt-get install libxvidcore-dev libx264-dev -y
+sudo apt-get install libgtk-3-dev -y
+sudo apt-get install libcanberra-gtk* -y
+sudo apt-get install libatlas-base-dev gfortran -y
+sudo apt-get install python3-dev -y
+
+cd ~
+wget -O opencv.zip https://github.com/opencv/opencv/archive/4.0.0.zip
+wget -O opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/4.0.0.zip 
+
+unzip opencv.zip
+unzip opencv_contrib.zip
+
+mv opencv-4.0.0 opencv
+mv opencv_contrib-4.0.0 opencv_contrib
+
+wget https://bootstrap.pypa.io/get-pip.py
+sudo python3 get-pip.py
+
+
